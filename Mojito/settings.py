48d464b360266105c769a10cf5012a52ce270a5e
Baatyr-4an <marisovbaatyr33@gmail.com>
2021-09-28 00:23:11 +0600

    also commit

--- Mojito/settings.py
+++ Mojito/settings.py
@@ -110,6 +110,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
